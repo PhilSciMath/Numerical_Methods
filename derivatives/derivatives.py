13481757d729fd32bdcafb0e6b9c84799d660886
PhilSciMath <leandro.ff7v2@gmail.com>
2025-01-14 11:11:04 -0300

    Updated derivatives.py and integrals.py

--- derivatives/derivatives.py
+++ derivatives/derivatives.py
@@ -1,30 +1,18 @@
 """
+    derivatives.py:
     A module containing numerical functions for derivatives. 
 """
 
-"""
-    Method 1 - Forward Difference.
-    The derivative function takes three arguments: a function f to derive,
-    a point x on which we want to take the derivative, and a value of h
-    which is the smallest delta x we can have.
-"""
 def fd(f, x, h):
+""" Derivative using Forward Difference """
     return (f(x + h) - f(x)) / h
 
 
-"""
-    Method 2 - Central-difference Approximation.
-    Same as before, it takes a function, an x value and an h value.
-"""
 def cd(f, x, h):
+""" Derivative using Central-Difference approximation """
     return (f(t + h) - f(t - h)) / 2 * h
 
 
-"""
-    Method 3 - Central-Difference for the Second Derivative. Here we 
-    implement the central-difference method for the second derivative 
-    because it is better than the forward difference method.
-    Same arguments again.
-"""
 def cdd(f, x, h):
+""" Second derivative with Central-Difference approximation """
     return (f(x - h) - 2 * f(x) + f(x + h)) / (h ** 2)
--- integrals/integrals.py
+++ integrals/integrals.py
@@ -1,4 +1,5 @@
 """
+    integrals.py:
     Here we define some functions for integrals using numerical methods.
     As with derivatives, delta x cannot go to infinity, so we must use a 
     limited value, which in most cases is h = (b - a) / N. The numerator 
@@ -6,30 +7,22 @@
     how many rectangles we want that interval divided into.
 """
 
-"""
-    Method 1 - Simple Integration.
-    Four arguments are required: a function f, the limits of integration a and
-    b, and the number of rectangles N to subdivide the interval [a,b].
-"""
+
 def sint(f, a, b, N):
+""" Integration by Riemann Sum """
     h = (b - a) / N
     return sum(f(a + i * h) * h for i in range(N))
 
 
-"""
-    Method 2 - Trapezoid Rule.
-    Same arguments as the previous one.
-"""
 def tint(f, a, b, N):
+""" Integration by the Trapezoid Rule """
     h = (b - a) / N
     value = sum(f(a + i * h) for i in range(1, N))
     return (f(a) + f(b) + 2 * value) * h / 2
 
 
-"""
-    Method 3 - Simpson's Rule.
-"""
 def simp(f, a, b, N):
+""" Integration by Simpson's Rule """
     if N % 2 != 0:
         raise ValueError("N must be even.")
 
